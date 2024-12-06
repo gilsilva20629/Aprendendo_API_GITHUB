f989286f9d1531705fe0c41649a5cebd95affd3e
gilsilva20629 <gilsilva20629@gmail.com>
2024-12-05 22:40:42 -0300

    consertando nome da funcao

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/my_endpoint', methods=['POST'])
-def processar():
+def home():
 
 	# ------------ Recepcao do request----------------
 	
