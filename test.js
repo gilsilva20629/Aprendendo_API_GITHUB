95376d196f1d58dd872fc0754baf5a2e2981846b
gilsilva20629 <gilsilva20629@gmail.com>
2025-09-16 18:04:07 -0300

    praticando html/javascript_002

--- test.js
+++ test.js
@@ -4,9 +4,24 @@ function validateForm(event){
 	event.preventDefault();	// Impede o envio imediato do formulário.
 	let form = event.target; // 'target' elemento que disparou o evento
 
-	console.log(form.parameters.name);
-	console.log(form.name);
-	console.log(form["name"]);
+	/*
+	A TRY declaração define um bloco de código a ser executado (para tentar).
+
+	A CATCH declaração define um bloco de código para lidar com qualquer erro.
+
+	A FINALLY instrução define um bloco de código a ser executado independentemente do resultado.
+
+	A THROW declaração define um erro personalizado.
+	*/
+
+	try{
+		console.log(form.parameters.name);
+		console.log(form.name);
+		console.log(form["name"]);
+	}
+	catch(err){
+		console.log(err.message)
+	}
 
 };
 
