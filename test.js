58f1b2d6fe282eaa28b0c342a75ce4fb6a88b4dd
gilsilva20629 <gilsilva20629@gmail.com>
2025-09-16 17:11:03 -0300

    ajustando index.html

--- test.js
+++ test.js
@@ -2,8 +2,9 @@
 
 function validateForm(event){
 	event.preventDefault();	// Impede o envio imediato do formulário.
-	let form = event.target;
+	let form = event.target; // 'target' elemento que disparou o evento
 
+	console.log(form.parameters.name);
 	console.log(form.name);
 	console.log(form["name"]);
 
